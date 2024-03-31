8f23d3ab5da9ac31511db55fb18a3b6e593517f5
Arthurhell <arthurfoiceiro@gmail.com>
2024-03-31 12:26:14 -0300

    Add project files.

---
+++ funssoes.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <locale.h>
+// Declaração da Função de Soma
+int SomaAUmNumero(int Numero, int NumeroAserSomado)
+{
+	// função return, usada para criar o processo de soma
+	// retornando os valores com o operador '+'
+	return Numero + NumeroAserSomado;
+}
+ nn
+int main()
+{
+	setlocale(LC_ALL, ("portuguese"));
+	int Numero, NumeroAserSomado;
+	std::cout << "Digite um número" << "\n";
+	std::cin >> Numero;
+	std::cout << "Digite o número a ser somado" << "\n";
+	std::cin >> NumeroAserSomado;
+	std::cout << "A soma do Número: " << Numero << " " << "Ao Número: " << NumeroAserSomado << " " << "É igual a: " << SomaAUmNumero(Numero, NumeroAserSomado);
+	return 0;
+}
